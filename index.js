32a07fed7815148b4f06f8c6f44886e1f8814012
Jake Gonchar <jake098877@gmail.com>
2020-04-03 15:21:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 var kittensTwo = ["Ralph"];
 kittens = kittens.concat(kittensTwo);
+kittens = ["Milo", "Otis", "Garfield"]
 // Add your functions and code here
