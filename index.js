aa9e063388df51dd46d0d43660f80249a4aec3ba
Jake Gonchar <jake098877@gmail.com>
2020-04-03 15:25:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 function destructivelyAppendKitten(kitten) {
   kittens.push(kitten);
 }
+function destructivelyPrependKitten(kitten) {
+  kittens.pop(kitten);
+}
 // Add your functions and code here
