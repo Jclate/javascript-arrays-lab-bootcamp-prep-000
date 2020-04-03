d2a25d7b9b3981eb6975ec8e743eac3a312bdb21
Jake Gonchar <jake098877@gmail.com>
2020-04-03 15:43:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,4 +19,10 @@ function appendKitten(name) {
   var kittens3 = kittens.concat(kitten2);
   return kittens3
 }
+
+function prependKitten(name) {
+  var kitten2 = [name]
+  var kittens3 = kitten2.concat(kittens)
+  return kittens3
+}
 // Add your functions and code here
